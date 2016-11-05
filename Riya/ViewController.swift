a7ae66391e96adbe4097008124af5170f46d4edf
thefreshduke <scottyshaw1@gmail.com>
2016-11-05 12:38:39 +1000

    Implemented button functionality to alternate background color between black and white

--- Riya/ViewController.swift
+++ Riya/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeBackgroundColor(sender: AnyObject) {
+        if self.view.backgroundColor != UIColor.blackColor() {
+            self.view.backgroundColor = UIColor.blackColor()
+        }
+        else {
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
